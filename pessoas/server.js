91364da6d72a74e27030de78d4a8b4a506ae5055
kyriosdata <fabio@inf.ufg.br>
2022-04-13 08:18:14 -0300

    audit

--- pessoas/server.js
+++ pessoas/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
-const { graphqlHTTP }= require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
-// Configuração para desenvolvimento (propriedade graphiql com i abaixo)
-// Visite a URL: http://localhost:4000/graphql
+// Configuração para desenvolvimento
+// (propriedade graphiql, observe o "i" em graphiql)
+// Visite a URL: http://localhost:4000/graphql (sem "i")
 
 const app = express();
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema : schema,
+    schema: schema,
     graphiql: true,
   })
 );
 
 app.listen(4000, () => {
-  console.log("Listening...");
+  console.log("Aguardando na porta 4000 ...");
 });
